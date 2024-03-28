80f856f0f3cf633a96f5de88b046c25501e77398
Brisstone <okolijohnson69@gmail.com>
2024-03-28 10:31:23 +0100

    OJ-signuop

--- src/pages/GoogleForm.tsx
+++ src/pages/GoogleForm.tsx
@@ -1,17 +1,55 @@
+import Logo from "../../public/seastrel-logo.jpeg";
+
 export default function GoogleForm() {
   return (
-    <div>
-      <div style={{ fontSize: "30px", fontWeight: 600 }}>
-        SEASTREL INTERNATIONAL LTD
-      </div>
-
-      <div className="card">
-        <button>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSf3v1Gl3Xu5Eqdy57tKdCo77-Esse3_JjuiuzC4MFxjbNu_Zw/viewform?usp=sf_link">
+    <div
+      style={{
+        // border: "2px solid red",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "300px",
+          // border: "2px solid red",
+        }}
+      >
+        <img
+          src={Logo}
+          alt="Seastrel Logo"
+          style={{ width: "100%", maxHeight: "300px" }}
+        />
+        <button
+          style={{
+            background: "skyblue",
+            color: "skyblue",
+            width: "100%",
+            marginTop: "20px",
+          }}
+        >
+          <a
+            style={{ color: "#051F44" }}
+            href="https://docs.google.com/forms/d/e/1FAIpQLSf3v1Gl3Xu5Eqdy57tKdCo77-Esse3_JjuiuzC4MFxjbNu_Zw/viewform?usp=sf_link"
+          >
             SIGN UP
           </a>
         </button>
       </div>
+      {/* <div style={{ fontSize: "25px", fontWeight: 600 }}>
+        SEASTREL INTERNATIONAL LTD
+      </div> */}
+
+      {/* <div className="card" style={{ maxWidth: "400px" }}>
+        <button
+          style={{ background: "skyblue", color: "white", width: "100%" }}
+        >
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSf3v1Gl3Xu5Eqdy57tKdCo77-Esse3_JjuiuzC4MFxjbNu_Zw/viewform?usp=sf_link">
+            SIGN UP
+          </a>
+        </button>
+      </div> */}
     </div>
   );
 }
