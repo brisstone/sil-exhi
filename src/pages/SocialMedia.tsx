06aec7b0db0d48534ee2d8b4e05ad1afe28adf63
Joynas <Ibinichinasajoy@gmail.com>
2024-03-28 01:45:55 +0100

    social-page-modification

--- src/pages/SocialMedia.tsx
+++ src/pages/SocialMedia.tsx
@@ -7,15 +7,15 @@ export default function SocialMedia() {
   return (
     <div>
       <div>
-        <div style={{cursor: 'pointer', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '30px',  }}>
-          <a href="https://www.facebook.com/seastrel" target="_blank" rel="noopener noreferrer" className="icon-hover " style={{ color: 'skyblue' }}>
-            <FaFacebook />
+        <div style={{cursor: 'pointer', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', gap: '50px',  }}>
+          <a href="https://www.facebook.com/seastrel" target="_blank" rel="noopener noreferrer" className="icon-hover " style={{ color: 'skyblue',  }}>
+            <FaFacebook size="40px"/>
           </a>
           <a href="https://www.instagram.com/seastrelhq" target="_blank" rel="noopener noreferrer" className="icon-hover" style={{ color: 'skyblue' }}>
-            <BsInstagram />
+            <BsInstagram size="40px"/>
           </a>
           <a href="https://twitter.com/seastrelhq" target="_blank" rel="noopener noreferrer" className="icon-hover" style={{ color: 'skyblue' }}>
-            <BsTwitterX /> 
+            <BsTwitterX size="40px"/> 
           </a>
         </div>
         <h1>SEASTREL INTERNATIONAL LTD</h1>
